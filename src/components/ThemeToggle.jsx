56cb276d2bcd3a9f5e7b05fd889fb126a8c77257
Juan Vivas <juanvivas043@gmail.com>
2025-08-09 21:30:22 -0400

    primera parte portafolio web

---
+++ src/components/ThemeToggle.jsx
@@ -0,0 +1,38 @@
+import { useState, useEffect } from "react"
+import { Sun, Moon } from "lucide-react"
+import { cn } from "@/lib/utils"
+
+export const ThemeToggle = () => {
+    const [isDarkMode, setDarkMode] = useState(false);
+
+    useEffect(() => {
+    const theme = window.localStorage.getItem('theme');
+
+    if (theme === 'dark') {
+        setDarkMode(true);
+        document.documentElement.classList.add('dark');
+    } else {
+        setDarkMode(false);
+    }
+
+    }, []);
+
+    const toggleTheme = () => {
+        isDarkMode === false ? document.documentElement.classList.add('dark') : document.documentElement.classList.remove('dark');
+        window.localStorage.setItem('theme', isDarkMode ? 'light' : 'dark');
+        setDarkMode(!isDarkMode);
+
+    }
+
+    return (
+        <button onClick={toggleTheme} className={cn("fixed max-sm:hidden top-5 right-5 z-50 p-2 rounded-full transition-colors duration-300",
+            "focus:outline-hidden",
+        )}>
+            
+
+            {isDarkMode ? <Sun className="h-6 w-6 text-yellow-300"/> 
+            : <Moon className="h-6 w-6 text-blue-900"/>}
+
+        </button>
+  )
+}
